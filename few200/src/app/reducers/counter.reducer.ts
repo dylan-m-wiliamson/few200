33ea82b9363319fec5131041be777a3324c1307d
dylan-m-wiliamson <82407179+dylan-m-wiliamson@users.noreply.github.com>
2021-05-10 21:02:44 +0000

    Initial commit

---
+++ few200/src/app/reducers/counter.reducer.ts
@@ -0,0 +1,22 @@
+import { state } from "@angular/animations";
+import { Action, createReducer, on } from "@ngrx/store";
+import * as actions from '../actions/counter.actions';
+
+export interface CounterState {
+  current: number
+}
+
+const initialState: CounterState = {
+  current: 0
+}
+
+const myReducer = createReducer(
+  initialState,
+  on(actions.countIncremented, (state) => ({ ...state, current: state.current + 1 })),
+  on(actions.countDecremented, (state) => ({ ...state, current: state.current - 1 })),
+  on()
+)
+
+export function reducer(state: CounterState = initialState, action: Action): CounterState {
+  return myReducer(state, action);
+}
---
+++ few200/src/app/reducers/index.ts
@@ -0,0 +1,9 @@
+import * as fromCounter from './counter.reducer';
+// This is for TypeScript. It just let's it know what is going on. None of this creates JavaScript
+export interface AppState {
+  counter: fromCounter.CounterState
+}
+
+export const reducers = {
+  counter: fromCounter.reducer
+}
