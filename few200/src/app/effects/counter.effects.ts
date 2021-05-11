08646fb322984252f377e9d9aa8f31ac7e99921a
dylan-m-wiliamson <82407179+dylan-m-wiliamson@users.noreply.github.com>
2021-05-11 17:42:55 +0000

    Update counter.effects.ts

--- few200/src/app/effects/counter.effects.ts
+++ few200/src/app/effects/counter.effects.ts
@@ -1,19 +1,35 @@
-import { Actions, createEffect } from "@ngrx/effects";
+import { Actions, createEffect, ofType } from "@ngrx/effects";
 import * as actions from '../actions/counter.actions';
 import { tap } from 'rxjs/operators';
+import { Injectable } from "@angular/core";
 
+@Injectable()
 export class CounterEffects {
 
-  // TODO: anytime counterCountBySet -> write to local storage.
+  // TODO: when the application starts -< read the value ->
+  // if it is there, dispatch a actions.counterCountBySet()
+  // if it is not there, we could dispatch counterCountBySet({by: 1})
+  // or we could do nothing
 
 
-
-
-  logEverything$ = createEffect(() =>
+  // TODO: any time counterCountBySet -> write it to local storage.
+  saveCountBy$ = createEffect(() =>
     this.actions$.pipe(
-      tap((action) => console.log(`Got an action of type ${action.type}`))
+      ofType(actions.counterCountBySet),
+      tap(a => localStorage.setItem('by', a.by.toString()))
     ), { dispatch: false }
   )
+
+
+
+
+  // logEverything$ = createEffect(() =>
+  //   this.actions$.pipe(
+  //     tap((action) => console.log(`Got an action of type ${action.type}`))
+  //   ), { dispatch: false }
+  // )
+
+
 
   constructor(private actions$: Actions) { }
 }
